34d9bcab89c522f9971f3b8eb7ee69e8bc050df3
skumar <skumar@634c09e3-b06c-4293-a491-0870fed93cb0>
2015-03-09 12:27:27 +0000

    Modified Constants in AudioPlayback
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@189864 634c09e3-b06c-4293-a491-0870fed93cb0

--- src/com/crestron/txrxservice/AudioPlayback.java
+++ src/com/crestron/txrxservice/AudioPlayback.java
@@ -8,74 +8,76 @@ import java.nio.ByteBuffer;
 
 public class AudioPlayback 
 {
-	public AudioManager mAudiomanager = null;
-	
-	AudioTrack mPlayer = null;
-	AudioRecord mRecorder = null;
-	String TAG = "TxRx AudioPlayback"; 
-	boolean shouldExit = false;
-	
+    public AudioManager mAudiomanager = null;
 
-	protected void startAudioTask(){
-		 new Thread(new StreamAudioTask()).start();
-	}
+    AudioTrack mPlayer = null;
+    AudioRecord mRecorder = null;
+    String TAG = "TxRx AudioPlayback"; 
+    boolean shouldExit = false;
 
-	class StreamAudioTask implements Runnable {
 
-		@Override
-			public void run() {
-				ByteBuffer readBuffer;
-				int audioFormat = 2;
-				int bufferSize = 131072;
-				int read = 0;
-				int readSize = bufferSize;
-				int sampleRate = 48000;
+    protected void startAudioTask(){
+        new Thread(new StreamAudioTask()).start();
+    }
 
-				Log.d(TAG, "Streaming Audio task started.... ");
-				try
-				{
-					bufferSize = AudioRecord.getMinBufferSize(sampleRate, 3, 2);
-					readBuffer = ByteBuffer.allocate(bufferSize);
-					mRecorder = new AudioRecord(5, sampleRate, 3, audioFormat, 4 * bufferSize);
-					mRecorder.startRecording();
-					mPlayer = new AudioTrack(3, sampleRate, 3, audioFormat, 4 * bufferSize, 1);
-					mPlayer.play();
-					readSize = bufferSize;
-					while(!shouldExit)
-					{
-						read = mRecorder.read(readBuffer.array(), 0, readSize);
-						if (read > 0)
-						{
-							mPlayer.write(readBuffer.array(), 0, read);
-							mPlayer.flush();
-						}
-					}
-				} catch (Exception localException) {
-					Log.e(TAG, "Audio exception caught");
-					localException.printStackTrace();
-					try{
-						Thread.sleep(100L);
-					}
-					catch (InterruptedException localInterruptedException)
-					{
-						localInterruptedException.printStackTrace();
-						mRecorder.stop();
-						mPlayer.stop();
-						mRecorder.release();
-						mPlayer.release();
-					}
-				}
-			}
-	}
-	
-	protected void stopAudioTask(){
-		Log.e(TAG, "stop Audio started");
-		shouldExit = true;
-		mRecorder.stop();
-		mPlayer.stop();
-		mRecorder.release();
-		mPlayer.release();
-		Log.e(TAG, "Audio Task Stopped");
-		shouldExit = false;
-	}
+    class StreamAudioTask implements Runnable {
+
+        public void run() {
+            ByteBuffer readBuffer;
+            final int audioFormat = 2;//ENCODING_PCM_!^BIT
+            final int audioChannels= 12;//CHANNEL_IN/OUT_STEREO
+            final int numOfBuffers= 4;
+            final int audioSource = 5; //Audio Source is CAMCORDER
+            int bufferSize = 131072;
+            int read = 0;
+            int readSize = bufferSize;
+            final int sampleRate = 48000;
+
+            Log.d(TAG, "Streaming Audio task started.... ");
+            try
+            {
+                bufferSize = AudioRecord.getMinBufferSize(sampleRate, audioChannels, audioFormat);
+                readBuffer = ByteBuffer.allocate(bufferSize);
+                mRecorder = new AudioRecord(audioSource, sampleRate, audioChannels, audioFormat, (numOfBuffers * bufferSize));
+                mRecorder.startRecording();
+                mPlayer = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, audioChannels, audioFormat, (numOfBuffers * bufferSize), AudioTrack.MODE_STREAM);
+                mPlayer.play();
+                readSize = bufferSize;
+                while(!shouldExit)
+                {
+                    read = mRecorder.read(readBuffer.array(), 0, readSize);
+                    if (read > 0)
+                    {
+                        mPlayer.write(readBuffer.array(), 0, read);
+                        mPlayer.flush();
+                    }
+                }
+            } catch (Exception localException) {
+                Log.e(TAG, "Audio exception caught");
+                localException.printStackTrace();
+                try{
+                    Thread.sleep(100L);
+                }
+                catch (InterruptedException localInterruptedException)
+                {
+                    localInterruptedException.printStackTrace();
+                    mRecorder.stop();
+                    mPlayer.stop();
+                    mRecorder.release();
+                    mPlayer.release();
+                }
+            }
+        }
+    }
+
+    protected void stopAudioTask(){
+        Log.e(TAG, "stop Audio started");
+        shouldExit = true;
+        mRecorder.stop();
+        mPlayer.stop();
+        mRecorder.release();
+        mPlayer.release();
+        Log.e(TAG, "Audio Task Stopped");
+        shouldExit = false;
+    }
 }
