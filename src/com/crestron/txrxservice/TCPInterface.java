3450b3231044c19e69df731f647e0de0b3253e05
rsecco <rsecco@634c09e3-b06c-4293-a491-0870fed93cb0>
2015-08-11 18:03:29 +0000

    Fix Bug 99972: Reverting change to streamurl feedback, was causing issues because not sent in update request
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@198120 634c09e3-b06c-4293-a491-0870fed93cb0

--- src/com/crestron/txrxservice/TCPInterface.java
+++ src/com/crestron/txrxservice/TCPInterface.java
@@ -357,9 +357,7 @@ public class TCPInterface extends AsyncTask<Void, Object, Long> {
 	        	    		tmp_str = parserInstance.processReceivedMessage(receivedMsg); 
 	        	        	
 	        		        try {
-	        		        	// FIXME: this is a hardcoded workaround, we should not just be echoing back
-	        		        	if (!tmp_str.toLowerCase().contains("streamurl"))
-	        		        		server.SendDataToAllClients(tmp_str);
+        		        		server.SendDataToAllClients(tmp_str);
 	        		        } catch (Exception e) {
 	        		            e.printStackTrace();
 	        		        }
