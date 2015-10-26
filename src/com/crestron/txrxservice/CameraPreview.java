8abcaf40bcd574863d81747841f84525e34ef486
rsecco <rsecco@634c09e3-b06c-4293-a491-0870fed93cb0>
2015-10-26 13:47:29 +0000

    Fix bug 103036: Added timeout to hdmi preview mode start/stop methods, similar to how we handle stream out start/stop
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@202057 634c09e3-b06c-4293-a491-0870fed93cb0

--- src/com/crestron/txrxservice/CameraPreview.java
+++ src/com/crestron/txrxservice/CameraPreview.java
@@ -2,6 +2,8 @@ package com.crestron.txrxservice;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import com.crestron.txrxservice.CresStreamCtrl.StreamState;
 
@@ -22,6 +24,8 @@ public class CameraPreview {
     boolean is_preview = false;
     boolean is_audioplaying = false;
     private boolean skipAudio = false;
+    private final long stopTimeout_ms = 15000;
+    private final long startTimeout_ms = 15000;
     //List<Camera.Size> mSupportedPreviewSizes;
     CresStreamCtrl streamCtl;
     private int idx = 0;
@@ -110,107 +114,150 @@ public class CameraPreview {
         return is_pause;
     }
 
-    public void startPlayback(boolean confidenceMode){
-        Log.d(TAG, "starting Playback " + is_preview);
-        if(is_preview == false){
-            Log.d(TAG, "Actual startPlayback");
-
-            CresCamera.openCamera();
-            // MNT - 3.10.15 
-            // getHdmiInputStatus causes a reset on the chip.  Calling this here causes
-            // the chip to get reset twice.  This will be fixed by Mistral.  However,
-            // until then, we will only call this on a resolution change or on startup.
-            //                if(mCamera!=null)
-            //                    hdmiinput = mCamera.getHdmiInputStatus();
-            if(CresCamera.mCamera != null){
-                try {
-                	CresCamera.mCamera.setPreviewDisplay(streamCtl.getCresSurfaceHolder(idx));
-                    //mCamera.setPreviewDisplay(surfaceHolder);
-                }catch (Exception localException) {
-                    localException.printStackTrace();
-                }
-
-                Camera.Parameters localParameters = CresCamera.mCamera.getParameters();
-                /*mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
-                  for (int i = 0; i < mSupportedPreviewSizes.size(); i++) {
-                  Log.d(TAG, i + ". Supported Resolution = " + mSupportedPreviewSizes.get(i).width + "x" + mSupportedPreviewSizes.get(i).height);
-                  }*/
-                if(CresStreamCtrl.hpdHdmiEvent==1){
-                    String resInfo = HDMIInputInterface.getHdmiInResolutionSysFs();//Reading From SysFs
-                    Log.i(TAG, "HDMI In Resolution API" + resInfo);
-                    hdmiIf.updateResolutionInfo(resInfo);
-                    CresStreamCtrl.hpdHdmiEvent=0;
-                }
-                if((Integer.parseInt(hdmiIf.getHorizontalRes())==0) && (Integer.parseInt(hdmiIf.getVerticalRes())==0)){
-                    //localParameters.setPreviewSize(640, 480);//if no hdmi cable is connected
-                    //mCamera.setDisplayOrientation(0);
-                    //mCamera.setParameters(localParameters);
-                    //mCamera.startPreview();
-                }
-                else{
-                    localParameters.setPreviewSize(Integer.parseInt(hdmiIf.getHorizontalRes()), Integer.parseInt(hdmiIf.getVerticalRes()));
-
-                    localParameters.set("ipp", "off");
-                    Log.d(TAG, "Preview Size set to " + localParameters.getPreviewSize().width + "x" + localParameters.getPreviewSize().height);
-                    CresCamera.mCamera.setDisplayOrientation(0);
-                    try {
-                    	CresCamera.mCamera.setParameters(localParameters);
-                    }catch (Exception localException) {
-                        localException.printStackTrace();
-                        localParameters.setPreviewSize(640, 480);
-                        CresCamera.mCamera.setParameters(localParameters);
-                    }
-                    CresCamera.mCamera.setPreviewCallback(new PreviewCB(confidenceMode));
-                    CresCamera.mCamera.setErrorCallback(new ErrorCB(confidenceMode));
-                    CresCamera.mCamera.startPreview();
-
-                	startAudio(); 
-					//Streamstate is now being fedback using preview callback                   
-                }
-                is_preview = true;
-            }
-            else
-            {
-            	stopPlayback(false);
-            }
-        }else   //Pause/Resume Case
-            resumePlayback(confidenceMode);      
+    public void startPlayback(final boolean confidenceMode){
+    	final CountDownLatch latch = new CountDownLatch(1);
+    	Thread startThread = new Thread(new Runnable() {
+    		public void run() {
+		        Log.d(TAG, "starting Playback " + is_preview);
+		        if(is_preview == false){
+		            Log.d(TAG, "Actual startPlayback");
+		
+		            CresCamera.openCamera();
+		            // MNT - 3.10.15 
+		            // getHdmiInputStatus causes a reset on the chip.  Calling this here causes
+		            // the chip to get reset twice.  This will be fixed by Mistral.  However,
+		            // until then, we will only call this on a resolution change or on startup.
+		            //                if(mCamera!=null)
+		            //                    hdmiinput = mCamera.getHdmiInputStatus();
+		            if(CresCamera.mCamera != null){
+		                try {
+		                	CresCamera.mCamera.setPreviewDisplay(streamCtl.getCresSurfaceHolder(idx));
+		                    //mCamera.setPreviewDisplay(surfaceHolder);
+		                }catch (Exception localException) {
+		                    localException.printStackTrace();
+		                }
+		
+		                Camera.Parameters localParameters = CresCamera.mCamera.getParameters();
+		                /*mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+		                  for (int i = 0; i < mSupportedPreviewSizes.size(); i++) {
+		                  Log.d(TAG, i + ". Supported Resolution = " + mSupportedPreviewSizes.get(i).width + "x" + mSupportedPreviewSizes.get(i).height);
+		                  }*/
+		                if(CresStreamCtrl.hpdHdmiEvent==1){
+		                    String resInfo = HDMIInputInterface.getHdmiInResolutionSysFs();//Reading From SysFs
+		                    Log.i(TAG, "HDMI In Resolution API" + resInfo);
+		                    hdmiIf.updateResolutionInfo(resInfo);
+		                    CresStreamCtrl.hpdHdmiEvent=0;
+		                }
+		                if((Integer.parseInt(hdmiIf.getHorizontalRes())==0) && (Integer.parseInt(hdmiIf.getVerticalRes())==0)){
+		                    //localParameters.setPreviewSize(640, 480);//if no hdmi cable is connected
+		                    //mCamera.setDisplayOrientation(0);
+		                    //mCamera.setParameters(localParameters);
+		                    //mCamera.startPreview();
+		                }
+		                else{
+		                    localParameters.setPreviewSize(Integer.parseInt(hdmiIf.getHorizontalRes()), Integer.parseInt(hdmiIf.getVerticalRes()));
+		
+		                    localParameters.set("ipp", "off");
+		                    Log.d(TAG, "Preview Size set to " + localParameters.getPreviewSize().width + "x" + localParameters.getPreviewSize().height);
+		                    CresCamera.mCamera.setDisplayOrientation(0);
+		                    try {
+		                    	CresCamera.mCamera.setParameters(localParameters);
+		                    }catch (Exception localException) {
+		                        localException.printStackTrace();
+		                        localParameters.setPreviewSize(640, 480);
+		                        CresCamera.mCamera.setParameters(localParameters);
+		                    }
+		                    CresCamera.mCamera.setPreviewCallback(new PreviewCB(confidenceMode));
+		                    CresCamera.mCamera.setErrorCallback(new ErrorCB(confidenceMode));
+		                    CresCamera.mCamera.startPreview();
+		
+		                	startAudio(); 
+							//Streamstate is now being fedback using preview callback                   
+		                }
+		                is_preview = true;
+		            }
+		            else
+		            {
+		            	stopPlayback(false);
+		            }
+		        }else   //Pause/Resume Case
+		            resumePlayback(confidenceMode);
+		        
+		        latch.countDown();
+    		}
+    	});
+    	startThread.start();        
+        
+        // We launch the start command in its own thread and timeout in case mediaserver gets hung
+    	boolean successfulStart = true; //indicates that there was no time out condition
+    	try { successfulStart = latch.await(startTimeout_ms, TimeUnit.MILLISECONDS); }
+    	catch (InterruptedException ex) { ex.printStackTrace(); }
+    	
+    	if (!successfulStart)
+    	{
+    		Log.e(TAG, String.format("Preview mode failed to start after %d ms", startTimeout_ms));
+    		startThread.interrupt(); //cleanly kill thread
+    		startThread = null;
+    		streamCtl.RecoverDucati();
+    	}
 
     }
 
-    public void stopPlayback(boolean confidenceMode)
+    public void stopPlayback(final boolean confidenceMode)
     {
-    	// TODO: ioctl crash when input hdmi is plugged in but no video passing
-        Log.d(TAG, "stopPlayback");
-        if(is_preview)
-        {
-            try
-            {
-                if (CresCamera.mCamera != null)
-                {
-                	CresCamera.mCamera.setPreviewCallback(null); //probably not necessary since handled by callback, but doesn't hurt
-                	CresCamera.mCamera.stopPreview();            		
-            		CresCamera.releaseCamera();
-                }
-                is_preview = false;
-                streamCtl.setPauseVideoImage(false);
-                is_pause = false;
-                Log.d(TAG, "Playback stopped !");
-                
-                if (!confidenceMode)
-                	streamCtl.SendStreamState(StreamState.STOPPED, idx);
-                
-            	stopAudio();
-            }
-            catch (Exception localException)
-            {
-                localException.printStackTrace();
-            }
-            
-            is_preview = false;
-        }
-        else
-            Log.d(TAG, "Playback already stopped");
+    	final CountDownLatch latch = new CountDownLatch(1);
+    	Thread stopThread = new Thread(new Runnable() {
+    		public void run() {
+		    	// TODO: ioctl crash when input hdmi is plugged in but no video passing
+		        Log.d(TAG, "stopPlayback");
+		        if(is_preview)
+		        {
+		            try
+		            {
+		                if (CresCamera.mCamera != null)
+		                {
+		                	CresCamera.mCamera.setPreviewCallback(null); //probably not necessary since handled by callback, but doesn't hurt
+		                	CresCamera.mCamera.stopPreview();            		
+		            		CresCamera.releaseCamera();
+		                }
+		                is_preview = false;
+		                streamCtl.setPauseVideoImage(false);
+		                is_pause = false;
+		                Log.d(TAG, "Playback stopped !");
+		                
+		                if (!confidenceMode)
+		                	streamCtl.SendStreamState(StreamState.STOPPED, idx);
+		                
+		            	stopAudio();
+		            }
+		            catch (Exception localException)
+		            {
+		                localException.printStackTrace();
+		            }
+		            
+		            is_preview = false;
+		        }
+		        else
+		            Log.d(TAG, "Playback already stopped");
+		        
+		        latch.countDown();
+    		}
+    	});
+    	stopThread.start();
+    	
+    	// We launch the stop commands in its own thread and timeout in case mediaserver gets hung
+    	boolean successfulStop = true; //indicates that there was no time out condition
+    	try { successfulStop = latch.await(stopTimeout_ms, TimeUnit.MILLISECONDS); }
+    	catch (InterruptedException ex) { ex.printStackTrace(); }
+    	
+    	if (!successfulStop)
+    	{
+    		Log.e(TAG, String.format("Preview mode failed to stop after %d ms", stopTimeout_ms));
+    		stopThread.interrupt(); //cleanly kill thread
+    		stopThread = null;
+    		streamCtl.SendStreamState(StreamState.STOPPED, idx);
+    		streamCtl.RecoverDucati();
+    	}
     }
 
     protected void startAudio(){
