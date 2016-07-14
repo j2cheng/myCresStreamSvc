43a20f65eb062c1182a714e73a86ad641df64308
jcheng <jcheng@634c09e3-b06c-4293-a491-0870fed93cb0>
2016-07-14 18:39:29 +0000

    set sefault frame rate to 30.
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@217384 634c09e3-b06c-4293-a491-0870fed93cb0

--- jni/cresStreamOut/cresStreamOut_Common_Def.h
+++ jni/cresStreamOut/cresStreamOut_Common_Def.h
@@ -7,7 +7,7 @@
 #define DEFAULT_RTSP_PORT "8554"
 #define DEFAULT_RES_X     "1280"
 #define DEFAULT_RES_Y     "720"
-#define DEFAULT_FRAME_RATE "15"
+#define DEFAULT_FRAME_RATE "30"
 
 #define MAX_STR_LEN    126
 #endif __CRESSTREAMOUTCOMMON_DEF_H__
