e3a0dd3bff5693685162bc907ebe7a9a23a103b1
jcheng <jcheng@634c09e3-b06c-4293-a491-0870fed93cb0>
2021-08-11 14:41:58 +0000

    Minor: print out disconnection list, which came from AVF. Code review:  Rakesh.
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@419020 634c09e3-b06c-4293-a491-0870fed93cb0

--- src/com/crestron/txrxservice/canvas/CanvasCrestore.java
+++ src/com/crestron/txrxservice/canvas/CanvasCrestore.java
@@ -1002,8 +1002,10 @@ public class CanvasCrestore
 					}
 				}
 			}
-			Common.Logging.i(TAG," playList: "+playList);
-			Common.Logging.i(TAG," stopList: "+stopList);
+			Common.Logging.i(TAG," playList: " + playList);
+			Common.Logging.i(TAG," stopList: " + stopList);
+                        Common.Logging.i(TAG," disconnectList: " + disconnectList);
+            
 			List<String> actionList = new LinkedList<String>();
 			for (int i=0; i < disconnectList.size(); i++)
 			{
