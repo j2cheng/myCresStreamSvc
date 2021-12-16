534c65adc8e59a9d6bed23075c6a21ef66474cf5
v-rhingorani <v-rhingorani@634c09e3-b06c-4293-a491-0870fed93cb0>
2021-12-16 19:40:50 +0000

    AM3K - go back to 16 char random username/pasword for WC.  CR: none
    git-svn-id: svn://linux2/subversion/Crestron/Android_tst600/trunk/customFiles/src/external/crestron/CresStreamSvc@437295 634c09e3-b06c-4293-a491-0870fed93cb0

--- jni/cresStreamOut/streamOutManager/cresStreamOutManager.cpp
+++ jni/cresStreamOut/streamOutManager/cresStreamOutManager.cpp
@@ -1363,8 +1363,8 @@ void CStreamoutManager::initWcCertificates()
 	if (m_auth_on && m_random_user_pw)
 	{
 	    // get random user name and password
-		setUsername((char *) genRandomString(4/*16*/).c_str());
-		setPassword((char *) genRandomString(8/*16*/).c_str());
+		setUsername((char *) genRandomString(16).c_str());
+		setPassword((char *) genRandomString(16).c_str());
 	}
     if (m_streamoutMode == STREAMOUT_MODE_WIRELESSCONFERENCING)
     {
